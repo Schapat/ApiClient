9d7d282209a92bc6d2521d246ec9894340cd4d89
Patrick S <68395365+Schapat@users.noreply.github.com>
2022-05-02 00:26:23 +0200

    re

--- Application/ProConfigRequest.cs
+++ Application/ProConfigRequest.cs
@@ -14,30 +14,15 @@ namespace ProConfigLoader
     {
         public async Task<List<ProConfig>> GetProConfig()
         {
-            ProConfig proConfig;
-            List<ProConfig> proConfigList = new List<ProConfig>();
             //Hier muss nachher der Login token uebergeben werden
             using (RestApi.client = new HttpClient())
             {
-
                 string url = RestApi.baseAddress + RestApi.proConfigAdress;
                 var response = await RestApi.client.GetAsync(url);
                 if (RestApi.IsConnected(response))
                 {
                     var responseString = await response.Content.ReadAsStringAsync();
                     var proConfigs = JsonConvert.DeserializeObject<List<ProConfig>>(responseString);
-
-                    foreach (var config in proConfigs)
-                    {
-                        proConfig = new ProConfig();
-                        proConfig.pro_id = config.pro_id;
-                        proConfig.pro_name = config.pro_name;
-                        proConfig.config = config.config;
-                        proConfig.autoexec = config.autoexec;
-                        proConfig.video = config.video;
-                        proConfigList.Add(proConfig);
-
-                    }
                     return proConfigs;
                 }
             }
